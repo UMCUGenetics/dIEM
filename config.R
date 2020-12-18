def2f0332ea3c57a2881b715d758d574438c1908
Ilja van Hoek <iljavanhoek2@gmail.com>
2020-12-18 18:18:13 +0100

    edit config structure and add path output

--- config.R
+++ config.R
@@ -1,35 +1,58 @@
 #file:config.R
 
-# name of the run
-run_name = "20201214_MAS_Melissa_DBA"
+
+# do not remove the quotes in this file, everything that is not a number
+# in this file should be in double quotes
+# every line with one or more #'s at the beginning is a comment and will not be 
+# read by R.
+
+
+
+# name of the run, will be the name of output folder and should not already exist 
+run_name = "20201023_Urines-3"
+
+# path: to DIMS excel file
+path_DIMSfile = "/Volumes/metab/Metabolomics/Research Metabolic Diagnostics/Metabolomics Projects/Projects 2020/Project 2020_010_SinglePatients_Urine/bioinformatics_URI_2020-10-23-urines_DIMS2.1.0_5ppm/URI_2020-20-23-urines_DIMS2.1.0_5ppm.xlsx"
+
+
+
+
 # binary variable: run function, yes(1) or no(0)
 algorithm = 1
 ratios = 1
 violin = 1
-# path: where should the outputfolder be made?
-path_output = "~/Documents/dIEM"
-# path: to DIMS excel file
-#path_DIMSfile = "/Volumes/LAB/metab/Metabolomics/Research Metabolic Diagnostics/Metabolomics Projects/Projects 2018/Project 2018_012 Algoritme_TestSet_DBS/RES_DBS_20181001_Run1_Algoritmetest/Bioinformatics_master_ppm5/RES_DBS_20181001_Run1_Algoritmetest_master_ppm5_filter.xlsx"
-path_DIMSfile = "/Volumes/LAB/metab/Metabolomics/Research Metabolic Diagnostics/Metabolomics Projects/Projects 2019/Project 2019_002 MAS/Metabolite plots_dIEM/20201214_DBA_Z-scoresformetaboliteplots.xlsx"
+
+# path: where should the outputfolder be made? Use your own output folder
+path_output = "~/Documents/dIEM/output"
+
+
+# standard input data files:
+
+
+### if violin = 1 
+# directory: folder in which all metabolite lists are (.txt)
+path_txtfiles = "~/github/dIEM/stofgroups"
+
+### if algorithm = 1
+path_expected = "~/github/dIEM/data/Expected_version20191108_CSV.csv"
+
+### if ratios = 1
+path_ratios = "~/github/dIEM/data/Ratios_20191108_CSV.csv"
+
+
+
+
+
+
+
+
+#### All settings below are not used yet.
+
+#top_diseases = 5
+#top_metab = 20
 # integer: are the sample names headers on row 1 or row 2 in the DIMS excel? (default 1)
 header_row = 1
 # column name where the data starts (default B)
 col_start = "B"
-
-### if violin = 1 
-# directory: folder in which all metabolite lists are (.txt)
-path_txtfiles = "~/Documents/dIEM/stofgroups"
 zscore_cutoff = 5
 xaxis_cutoff = 20
-
-
-### if algorithm = 1
-path_expected = "~/Documents/dIEM/Expected_version20191108_CSV.csv"
-
-### if ratios = 1
-path_ratios = "~/Documents/dIEM/Ratios_20191108_CSV.csv"
-
-
-### if algoritm = 1 & violin = 1
-#top_diseases = 5
-#top_metab = 20
