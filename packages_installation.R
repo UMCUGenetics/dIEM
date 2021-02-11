521870f5b2f3c8047ea798922d90d4fab0adbcd7
Ilja van Hoek <iljavanhoek2@gmail.com>
2021-02-11 17:35:35 +0100

    add tidyr to packages to install

--- packages_installation.R
+++ packages_installation.R
@@ -1,4 +1,4 @@
-list.of.packages <- c("beepr", "dplyr", "reshape2", "openxlsx", "ggplot2", "gghighlight", "data.table")
+list.of.packages <- c("beepr", "dplyr", "reshape2", "openxlsx", "ggplot2", "gghighlight", "data.table", "tidyr")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 
