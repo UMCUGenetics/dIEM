7a472aa527a2a16227bd30681248a1235a7af744
Ilja van Hoek <iljavanhoek2@gmail.com>
2021-03-01 12:15:02 +0100

    config settings with new dataversions: exp & ratios

--- config.R
+++ config.R
@@ -9,10 +9,41 @@
 
 
 # name of the run, will be the name of output folder and should not already exist 
-run_name = "test1-windows-birgitpc"
+run_name = "test_slicemaxmin"
 
 # path: to DIMS excel file
-path_DIMSfile = "Y:/Untargeted\ Metabolomics/Algoritme/dIEM/voorbeeld_PL_Coco_Nodding_RUN1.xlsx"
+#path_DIMSfile = "Y:/Untargeted\ Metabolomics/Algoritme/dIEM/voorbeeld_PL_Coco_Nodding_RUN1.xlsx"
+
+path_DIMSfile = "/Users/ihoek3/Documents/dIEM/voorbeeld_PL_Diagn17_RUN10.xlsx"
+
+
+# path: where should the outputfolder be made? Use your own output folder
+#path_output = "Y:/Untargeted\ Metabolomics/Algoritme/dIEM/test-output"
+path_output = "/Users/ihoek3/Documents/dIEM/output"
+
+# standard input data files:
+
+
+### if violin = 1 
+# directory: folder in which all metabolite lists are (.txt)
+#path_txtfiles = "Y:/Untargeted\ Metabolomics/Algoritme/dIEM/dIEM-main/stofgroups"
+path_txtfiles = "/Users/ihoek3/github/dIEM/stofgroups"
+### if algorithm = 1
+#path_expected = "Y:/Untargeted\ Metabolomics/Algoritme/dIEM/dIEM-main/data/Expected_version20191108_CSV.csv"
+path_expected = "/Users/ihoek3/github/dIEM/data/Expected_version2020_CSV.csv"
+### if ratios = 1
+#path_ratios = "Y:/Untargeted\ Metabolomics/Algoritme/dIEM/dIEM-main/data/Ratios_20191108_CSV.csv"
+path_ratios = "/Users/ihoek3/github/dIEM/data/Ratios_20210216_CSV.csv"
+
+
+
+
+
+
+
+
+
+####### All settings below are not used yet.
 
 
 
@@ -21,33 +52,6 @@ path_DIMSfile = "Y:/Untargeted\ Metabolomics/Algoritme/dIEM/voorbeeld_PL_Coco_No
 algorithm = 1
 ratios = 1
 violin = 1
-
-# path: where should the outputfolder be made? Use your own output folder
-path_output = "Y:/Untargeted\ Metabolomics/Algoritme/dIEM/test-output"
-
-
-# standard input data files:
-
-
-### if violin = 1 
-# directory: folder in which all metabolite lists are (.txt)
-path_txtfiles = "Y:/Untargeted\ Metabolomics/Algoritme/dIEM/dIEM-main/stofgroups"
-
-### if algorithm = 1
-path_expected = "Y:/Untargeted\ Metabolomics/Algoritme/dIEM/dIEM-main/data/Expected_version20191108_CSV.csv"
-
-### if ratios = 1
-path_ratios = "Y:/Untargeted\ Metabolomics/Algoritme/dIEM/dIEM-main/data/Ratios_20191108_CSV.csv"
-
-
-
-
-
-
-
-
-#### All settings below are not used yet.
-
 #top_diseases = 5
 #top_metab = 20
 # integer: are the sample names headers on row 1 or row 2 in the DIMS excel? (default 1)
