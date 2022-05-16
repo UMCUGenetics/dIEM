859d1e1482c0779d40449d82260ebc7499c54c22
Ilja van Hoek <iljavanhoek2@gmail.com>
2022-05-16 14:38:11 +0200

    packages gridExtra and grid added

--- packages_installation.R
+++ packages_installation.R
@@ -1,4 +1,4 @@
-list.of.packages <- c("beepr", "dplyr", "reshape2", "openxlsx", "ggplot2", "gghighlight", "data.table", "tidyr")
+list.of.packages <- c("beepr", "dplyr", "reshape2", "openxlsx", "ggplot2", "gghighlight", "data.table", "tidyr", "gridExtra", "grid")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 
